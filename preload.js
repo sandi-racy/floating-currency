887e9d61814785f3d013b7bc518743d5ec78e4c8
sandi_rosyandi <sandiracyyandi@gmail.com>
2020-04-10 17:00:51 +0700

    Initial commit

--- preload.js
+++ preload.js
@@ -1,12 +1,29 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+const numberFormat = require('./js/number-format')
+const { render: timeago } = require('timeago.js')
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+function getData () {
+	fetch('https://api.exchangeratesapi.io/latest?base=USD')
+  .then((response) => response.json())
+  .then((data) => {
+  	let idr = numberFormat(data.rates.IDR)
+  	document.getElementById('fc--item').innerHTML = `
+  		<span class="fc--currency">USD 1</span>
+      <span class="fc--equal">=</span>
+      <span class="fc--currency">IDR ${idr}</span>
+      <span class="fc--time" id="fc--time" datetime="${new Date()}"></span>
+      <img src="./images/currency.png" class="fc--currency-image">
+  	`
+  	timeago(document.getElementById('fc--time'))
+  })
+  .catch(() => {
+  	document.getElementById('fc--item').innerHTML = `<div class="fc--message__error">Can not get data from the API</div>`
+  });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+	setInterval(() => {
+		getData()
+	}, 180000)
+
+  getData()
 })
